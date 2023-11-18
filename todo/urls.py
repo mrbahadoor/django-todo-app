c14c0dab83f42d79828093fe0029eb5b89ae7dc8
mrbahadoor <mrbahadoor@gmail.com>
2023-11-18 23:04:16 +0400

    Handle edit-task route with GET request

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
+    
+    # Mark as done
+    path('mark-as-done/<int:pk>/', views.markAsDone, name='markAsDone'),
+
+    # Mark as undone
+    path('mark-as-undone/<int:pk>/', views.markAsDone, name='markAsUndone'),
+
+    # EDIT feature
+    path('edit-task/<int:pk>/', views.editTask, name='editTask'),
 ]
