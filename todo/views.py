7d4be3f058b59f387cdd8ec2b979cd2c5e4577f9
mrbahadoor <mrbahadoor@gmail.com>
2023-11-18 21:50:12 +0400

    Add form POST on create task plus CSRF TOKEN

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Task
 
 # Create your views here.
+
+def addTask(request):
+    #For testing route
+    # return HttpResponse('The form is submitted')
+
+    # Retrieve POST data
+    # print(request.POST['task'])
+    task = request.POST['task']
+
+    #Create task object
+    Task.objects.create(task=task)
+
+    return redirect('home')    
